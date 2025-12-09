f439f15938d0da63c48c19d6a9b88db22a76a073
Jose Saldivia <antoniogb.saldivia@gmail.com>
2025-12-09 20:01:39 -0300

    Taller1_ProgramacionHibrida_JASG

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.taller1josesaldiviag',
+  appName: 'taller1josesaldiviag',
+  webDir: 'www'
+};
+
+export default config;
