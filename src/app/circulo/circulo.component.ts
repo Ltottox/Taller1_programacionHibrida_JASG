b325fd757a499c24b4a07c6dccf24571329b1874
Jose Saldivia <antoniogb.saldivia@gmail.com>
2025-12-10 17:26:48 -0300

    listo circulo

--- src/app/circulo/circulo.component.ts
+++ src/app/circulo/circulo.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/angular/standalone';
-
+import {  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonButton, IonImg, IonText } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Circulo } from '../clases/circulo';
 
 @Component({
   selector: 'app-circulo',
   templateUrl: './circulo.component.html',
   styleUrls: ['./circulo.component.scss'],
   standalone: true,
-  imports: [IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle], //se repite imports en un array
+  imports: [IonText, CommonModule, FormsModule, IonImg, IonInput, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle], //se repite imports en un array
 })
 export class CirculoComponent  implements OnInit {
+  radio: number = 0;
+  resultado: number | null = null;
+  mostrarResultado: boolean = false;
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {}
 
+  calcularPerimetro() {
+    const circulo = new Circulo(this.radio);
+    this.resultado = circulo.calcularPerimetro();
+    this.mostrarResultado = true;
+  }
+
 }
+
+
