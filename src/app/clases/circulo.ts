a8a80e898c782afe1df0265603b5310a4706ca6d
Jose Saldivia <antoniogb.saldivia@gmail.com>
2025-12-09 21:16:05 -0300

    extiende FG a circulo

--- src/app/clases/circulo.ts
+++ src/app/clases/circulo.ts
@@ -1,2 +1,14 @@
-export class Circulo {
+import { FiguraGeomerica } from "./figura-geomerica";
+//clase Circulo que extiende de FiguraGeomerica
+export class Circulo extends FiguraGeomerica {
+    radio: number;
+
+    constructor(radio: number) {
+        super();
+        this.radio = radio;
+    }
+
+    public calcularPerimetro(): number {
+        return 2 * Math.PI * this.radio;
+    }
 }
--- src/app/clases/figura-geomerica.ts
+++ src/app/clases/figura-geomerica.ts
@@ -1,2 +1,4 @@
-export class FiguraGeomerica {
+//metodo abstracto para calcular el perímetro de una figura geométrica
+export abstract class FiguraGeomerica {
+    public abstract calcularPerimetro(): number;
 }
