3c2779282bc40d2f816ea98921d1ae8664ce79ab
Jose Saldivia <antoniogb.saldivia@gmail.com>
2025-12-17 12:51:12 -0300

    agrega Ion-select para separar trinagulo de circulo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSelect,IonSelectOption } from '@ionic/angular/standalone';
 import { CirculoComponent } from "../circulo/circulo.component";
 import { TrianguloComponent } from "../triangulo/triangulo.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CirculoComponent, TrianguloComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CirculoComponent, TrianguloComponent, IonSelect,IonSelectOption, CommonModule],
 })
 export class HomePage {
+  seleccionFigura: 'circulo' | 'triangulo' = 'circulo';
   constructor() {}
+
+  esCirculo(){return this.seleccionFigura=='circulo'}
+  esTriangulo(){return this.seleccionFigura=='triangulo'}
+  manejoSeleccionFigura($event: any) {
+    this.seleccionFigura = $event.detail.value;
+  }
 }
